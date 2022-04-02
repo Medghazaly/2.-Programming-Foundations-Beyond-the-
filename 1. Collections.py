0a932cc3c81eae72eb2986495aff8a3bb84e5376
ghazaly <medghazalyegy@gmail.com>
2022-04-02 13:50:22 +0000

    Upload files

---
+++ 1. Collections.py
@@ -0,0 +1,74 @@
+# Collections
+
+
+# List
+
+# city1 = 'Nouakchott'
+# city2 = 'Dakar'
+# city3 = 'Algeia'
+# city4 = 'Cairo'
+
+# I collected them all in one container
+cities = [
+    'Nouakchott', 
+    'Dakar', 
+    'Algeia', 
+    'Cairo',
+]
+
+print(cities[1]) # This will print second text from Cities (Dakar)
+
+# Dictionary
+
+# California state symbols
+# state_insect = 'Butterfly'
+# state_animal = 'Bear'
+# state_flower = 'Poppy'
+# state_fruit = 'Strawberry'
+
+california_symbols = {
+    'insect': 'Butterfly',
+    'animal': 'Bear',
+    'flower': 'Poppy',
+    'fruit': 'Strawberry',
+}
+
+print(california_symbols['fruit'])
+
+# Solution of Challenge
+
+# Nearest stars to Earth
+# star1 = 'Sol'
+# star2 = 'Alpha Centauri'
+# star3 = 'Barnard'
+# star4 = 'Wolf 359'
+
+stars = [
+    'Sol',
+    'Alpha Centauri',
+    'Barnard',
+    'Wolf 359',
+]
+
+print(stars[3])
+
+# Highest peak on each tectonic plate
+# African = 'Kilimanjaro'
+# Antarctic = 'Vinson'
+# Australian = 'Puncak Jaya'
+# Eurasian = 'Everest'
+# North_American = 'Denali'
+# Pacific = 'Mauna Kea'
+# South_American = 'Aconcagua'
+
+peaks = {
+    'African': 'Kilimanjaro',
+    'Antarctic': 'Vinson',
+    'Australian': 'Puncak Jaya',
+    'Eurasian': 'Everest',
+    'North_American': 'Denali',
+    'Pacific': 'Mauna Kea',
+    'South_American': 'Aconcagua',
+}
+
+print(peaks['Pacific'])
---
+++ 2. Iteration.py
@@ -0,0 +1,41 @@
+# Iteration
+
+# Loop
+
+spices = [
+    'Pasta',
+    'Salt',
+    'pepper',
+    'egg',
+    'Water',
+]
+
+# for Codition
+for spice in spices:
+    print(spice)
+print('No more boring Pasta!')
+
+
+# while Condition
+
+print('Counting to 100 by fives:')
+i = 5
+while i <= 100:
+    print(i)
+    i += 5
+print('List complete!')
+
+# Solution of challenge
+
+fruits = [
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangos',
+    'nectarines',
+    'pears',
+]
+
+print('Our fruit selection:')
+for fruit in fruits:
+    print(fruit)
